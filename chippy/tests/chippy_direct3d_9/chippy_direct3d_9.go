e78088aa1921e27092fa912f61a674ed790dca1e
stephen.gutekanst <stephen.gutekanst@gmail.com>
2013-08-11 06:48:12 -0700

    Chippy: Remove Direct3D unimplemented tests and source files.

--- chippy/tests/chippy_direct3d_9/chippy_direct3d_9.go
+++
@@ -1,6 +0,0 @@
-// Copyright 2012 Lightpoke. All rights reserved.
-// This source code is subject to the terms and
-// conditions defined in the "License.txt" file.
-
-// Test application - Opens multiple windows, uses Direct3D 9 rendering in each of them
-package main
