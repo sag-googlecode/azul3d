59b597107e1afb2d3fbd672bbf9d2937312b6fd9
stephen.gutekanst <stephen.gutekanst@gmail.com>
2013-09-20 14:38:30 -0700

    binpack: Operate on Packable interface instead of fixed grid.

--- binpack/binpack.go
+++ binpack/binpack.go
@@ -2,105 +2,98 @@
 // This source code is subject to the terms and
 // conditions defined in the "License.txt" file.
 
-package binpack
-
-import (
-	"errors"
-)
-
-type Block struct {
-	Width, Height uint
-	X, Y          uint
-}
-
-type Grid struct {
-	Width, Height uint
-}
-
-// Pack is an 2D bin packing algorithm based on the work of Jake Gordon, described on his blog at:
+// Package binpack implements 2D bin packing using Jake Gordon's algorithm.
+//
+// The algorithm used is described on Jake's blog here:
 //
 //   http://codeincomplete.com/posts/2011/5/7/bin_packing/
 //
-// And implemented by him in JavaScript here:
+// And is also implemented by him in JavaScript here:
 //
 //   https://github.com/jakesgordon/bin-packing
 //
-// The returned grid will reflect how large of an grid is required to fit all blocks.
+package binpack
+
+type Packable interface {
+	// Len should return the number of blocks in total.
+	Len() int
+
+	// Size should return the width and height of the block n
+	Size(n int) (width, height int)
+
+	// Place should place the block n, at the position [x, y].
+	Place(n, x, y int)
+}
+
+// Pack uses the packable interface, p, to pack two dimensional blocks onto a
+// larger two dimensional grid.
 //
-// The returned slice of blocks will have their X and Y coordinates updated to reflect where the
-// block can be placed efficiently on the grid.
+// The algorithm does not start with an fixed width and height, instead it
+// starts with the width and height of the first block and then grows as
+// neccessary to fit each block into the overall grid. As the grid is grown the
+// algorithm attempts to maintain an roughly square ratio by making 'smart'
+// choices about whether to grow right or down.
 //
-// The algorithm does not start with an fixed width and height, instead it starts with the width
-// and height of the first block in the slice and then grows as neccessary to fit each block. As
-// the grid is grown it attempts to maintain an roughly square ratio by making 'smart' choices
-// about whether to grow right or down.
+// The returned width and height reflect how large the overall grid must be to
+// contain at least each packed block.
 //
-// When growing, the algorithm can only grow to the right OR down. Therefore, if the new block is
-// BOTH wider and taller than the current target then it will be rejected. This makes it very
-// important to initialize with a sensible starting width and height. If you are providing sorted
-// input (largest first) then this will not be an issue.
+// When growing, the algorithm can only grow to the right or down. If the new
+// block is both wider and taller than the first [p.Size(0)] block, then the
+// algorithm will be unable to pack the blocks, and [-1, -1] will be returned.
 //
-// A potential way to solve this limitation would be to allow growth in BOTH directions at once,
-// but this requires maintaining a more complex tree with 3 children (down, right and center) and
-// that complexity can be avoided by simply chosing a sensible starting block.
+// To avoid the above problem, sort blocks by max(width, height).
 //
-// Best results occur when the input blocks are sorted by height, or even better when sorted by
-// max(width,height).
-//
-// This function will panic if len(blocks) == 0, or if any of the *Block in the slice are nil.
-func Pack(blocks []*Block) (*Block, error) {
-	if len(blocks) == 0 {
-		panic("Pack(): Cannot pack zero blocks!")
+// If the number of blocks is zero (p.Len() == 0) then this function is no-op
+// and [0, 0] is returned.
+func Pack(p Packable) (width, height int) {
+	numBlocks := p.Len()
+
+	if numBlocks == 0 {
+		return 0, 0
 	}
 
+	w, h := p.Size(0)
 	root := &node{
 		x:      0,
 		y:      0,
-		width:  blocks[0].Width,
-		height: blocks[0].Height,
+		width:  w,
+		height: h,
 	}
 
-	for _, block := range blocks {
-		if block == nil {
-			panic("Pack(): Block is nil!")
-		}
+	p.Place(0, 0, 0)
 
-		node := root.find(block.Width, block.Height)
+	for i := 0; i < numBlocks; i++ {
+		w, h = p.Size(i)
+
+		node := root.find(w, h)
 		if node != nil {
-			node = node.split(block.Width, block.Height)
+			node = node.split(w, h)
 
 			// Update block in-place
-			block.X = node.x
-			block.Y = node.y
+			p.Place(i, node.x, node.y)
 
 		} else {
-			newRoot, grown := root.grow(block.Width, block.Height)
+			newRoot, grown := root.grow(w, h)
 			if newRoot == nil {
-				return nil, errors.New("Pack(): Could not grow node, first block size is not large enough.")
+				return -1, -1
 			}
 
 			// Update block in-place
-			block.X = grown.x
-			block.Y = grown.y
+			p.Place(i, grown.x, grown.y)
 
 			root = newRoot
 		}
 	}
 
-	bounding := &Block{
-		Width:  root.width,
-		Height: root.height,
-	}
-
-	return bounding, nil
+	return root.width, root.height
 }
 
 type node struct {
-	x, y, width, height uint
+	x, y, width, height int
 	right, down         *node
 }
 
-func (n *node) find(width, height uint) *node {
+func (n *node) find(width, height int) *node {
 	if n.right != nil || n.down != nil {
 		right := n.right.find(width, height)
 		if right != nil {
@@ -113,7 +106,7 @@ func (n *node) find(width, height uint) *node {
 	return nil
 }
 
-func (n *node) split(width, height uint) *node {
+func (n *node) split(width, height int) *node {
 	n.down = &node{
 		x:      n.x,
 		y:      n.y + height,
@@ -131,7 +124,7 @@ func (n *node) split(width, height uint) *node {
 	return n
 }
 
-func (n *node) grow(width, height uint) (root, grown *node) {
+func (n *node) grow(width, height int) (root, grown *node) {
 	canGrowDown := width <= n.width
 	canGrowRight := height <= n.height
 
@@ -155,7 +148,7 @@ func (n *node) grow(width, height uint) (root, grown *node) {
 	return nil, nil
 }
 
-func (n *node) growRight(width, height uint) (root, grown *node) {
+func (n *node) growRight(width, height int) (root, grown *node) {
 	newRoot := &node{
 		x:      0,
 		y:      0,
@@ -177,7 +170,7 @@ func (n *node) growRight(width, height uint) (root, grown *node) {
 	return nil, nil
 }
 
-func (n *node) growDown(width, height uint) (root, grown *node) {
+func (n *node) growDown(width, height int) (root, grown *node) {
 	newRoot := &node{
 		x:      0,
 		y:      0,
