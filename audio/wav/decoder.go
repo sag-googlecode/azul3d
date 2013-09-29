0ee7bd8faed0aa52b10bace84635692015e6bff2
stephen.gutekanst <stephen.gutekanst@gmail.com>
2013-09-29 01:41:10 -0700

    audio/wav: go fmt

--- audio/wav/decoder.go
+++ audio/wav/decoder.go
@@ -7,10 +7,10 @@ package wav
 import (
 	"code.google.com/p/azul3d/audio"
 	"encoding/binary"
-	"unsafe"
 	"errors"
-	"sync"
 	"io"
+	"sync"
+	"unsafe"
 )
 
 const (
@@ -35,9 +35,9 @@ const (
 type decoder struct {
 	access sync.RWMutex
 
-	format, bitsPerSample uint16
+	format, bitsPerSample   uint16
 	chunkSize, currentCount uint32
-	dataChunkBegin int32
+	dataChunkBegin          int32
 
 	r      interface{}
 	rd     io.Reader
@@ -85,7 +85,7 @@ func (d *decoder) Seek(sample uint64) error {
 	rs, ok := d.r.(io.ReadSeeker)
 	if ok {
 		offset := int64(sample * (uint64(d.bitsPerSample) / 8))
-		_, err := rs.Seek(int64(d.dataChunkBegin) + offset, 0)
+		_, err := rs.Seek(int64(d.dataChunkBegin)+offset, 0)
 		if err != nil {
 			return err
 		}
@@ -166,7 +166,6 @@ func (d *decoder) readPCM24(b audio.Buffer) (read int, err error) {
 
 	return
 }
-
 
 func (d *decoder) readPCM32(b audio.Buffer) (read int, err error) {
 	bb, bbOk := b.(audio.PCM32Samples)
@@ -349,7 +348,7 @@ func newDecoder(r interface{}) (audio.Decoder, error) {
 		panic("NewDecoder(): Invalid reader type; must be io.Reader or io.ReadSeeker!")
 	}
 
-	var(
+	var (
 		complete bool
 
 		c16 fmtChunk16
@@ -363,74 +362,74 @@ func newDecoder(r interface{}) (audio.Decoder, error) {
 		}
 
 		switch ident {
-			case "RIFF":
-				var format [4]byte
-				err = d.bRead(&format, unsafe.Sizeof(format))
-				if string(format[:]) != "WAVE" {
-					return nil, audio.ErrInvalidData
-				}
+		case "RIFF":
+			var format [4]byte
+			err = d.bRead(&format, unsafe.Sizeof(format))
+			if string(format[:]) != "WAVE" {
+				return nil, audio.ErrInvalidData
+			}
 
-			case "fmt ":
-				// Always contains the 16-byte chunk
-				err = d.bRead(&c16, unsafe.Sizeof(c16))
+		case "fmt ":
+			// Always contains the 16-byte chunk
+			err = d.bRead(&c16, unsafe.Sizeof(c16))
+			if err != nil {
+				return nil, err
+			}
+			d.bitsPerSample = to16(c16.BitsPerSample[:])
+
+			// Sometimes contains extensive 18/40 total byte chunks
+			switch length {
+			case 18:
+				err = d.bRead(&c18, unsafe.Sizeof(c18))
 				if err != nil {
 					return nil, err
 				}
-				d.bitsPerSample = to16(c16.BitsPerSample[:])
-
-				// Sometimes contains extensive 18/40 total byte chunks
-				switch length {
-				case 18:
-					err = d.bRead(&c18, unsafe.Sizeof(c18))
-					if err != nil {
-						return nil, err
-					}
-				case 40:
-					err = d.bRead(&c40, unsafe.Sizeof(c40))
-					if err != nil {
-						return nil, err
-					}
-				}
-
-				// Verify format tag
-				ft := to16(c16.FormatTag[:])
-				switch {
-				case ft == wave_FORMAT_PCM && (d.bitsPerSample == 8 || d.bitsPerSample == 16 || d.bitsPerSample == 24 || d.bitsPerSample == 32):
-					break
-				case ft == wave_FORMAT_IEEE_FLOAT && (d.bitsPerSample == 32 || d.bitsPerSample == 64):
-					break
-				case ft == wave_FORMAT_ALAW && d.bitsPerSample == 8:
-					break
-				case ft == wave_FORMAT_MULAW && d.bitsPerSample == 8:
-					break
-				// We don't support extensible wav files
-				//case wave_FORMAT_EXTENSIBLE:
-				//	break
-				default:
-					return nil, ErrUnsupported
-				}
-
-				// Assign format tag for later (See Read() method)
-				d.format = to16(c16.FormatTag[:])
-
-				// We now have enough information to build the audio configuration
-				d.config = &audio.Config{
-					Channels:   int(to16(c16.Channels[:])),
-					SampleRate: int(to32(c16.SamplesPerSec[:])),
-				}
-
-			case "fact":
-				// We need to scan fact chunk first.
-				var fact factChunk
-				err = d.bRead(&fact, unsafe.Sizeof(fact))
+			case 40:
+				err = d.bRead(&c40, unsafe.Sizeof(c40))
 				if err != nil {
 					return nil, err
 				}
+			}
 
-			case "data":
-				// Read the data chunk header now
-				d.chunkSize = length
-				complete = true
+			// Verify format tag
+			ft := to16(c16.FormatTag[:])
+			switch {
+			case ft == wave_FORMAT_PCM && (d.bitsPerSample == 8 || d.bitsPerSample == 16 || d.bitsPerSample == 24 || d.bitsPerSample == 32):
+				break
+			case ft == wave_FORMAT_IEEE_FLOAT && (d.bitsPerSample == 32 || d.bitsPerSample == 64):
+				break
+			case ft == wave_FORMAT_ALAW && d.bitsPerSample == 8:
+				break
+			case ft == wave_FORMAT_MULAW && d.bitsPerSample == 8:
+				break
+			// We don't support extensible wav files
+			//case wave_FORMAT_EXTENSIBLE:
+			//	break
+			default:
+				return nil, ErrUnsupported
+			}
+
+			// Assign format tag for later (See Read() method)
+			d.format = to16(c16.FormatTag[:])
+
+			// We now have enough information to build the audio configuration
+			d.config = &audio.Config{
+				Channels:   int(to16(c16.Channels[:])),
+				SampleRate: int(to32(c16.SamplesPerSec[:])),
+			}
+
+		case "fact":
+			// We need to scan fact chunk first.
+			var fact factChunk
+			err = d.bRead(&fact, unsafe.Sizeof(fact))
+			if err != nil {
+				return nil, err
+			}
+
+		case "data":
+			// Read the data chunk header now
+			d.chunkSize = length
+			complete = true
 		}
 	}
 
