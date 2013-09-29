33918ea56fb3a7dd09f4e78d1148ca20f3196581
stephen.gutekanst <stephen.gutekanst@gmail.com>
2013-09-29 01:26:35 -0700

    chippy: remove unused tests.

--- chippy/tests/chippy_screens_query/chippy_screens_query.go
+++ chippy/tests/chippy_screens_query/chippy_screens_query.go
@@ -1,8 +1,9 @@
 // Copyright 2012 Lightpoke. All rights reserved.
 // This source code is subject to the terms and
 // conditions defined in the "License.txt" file.
+// +build tests
 
-// Test application - Queries for all available screens, lists and changes screen modes
+// Test - Queries and lists all available screens, and their modes.
 package main
 
 import (
