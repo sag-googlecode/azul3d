9823446a3c1ccf9728dc3a79f53a5b0f5aa28a80
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2013-11-22 11:57:07 -0700

    chippy: update GLSetVerticalSync doc (fixes issue 17); column width to 80

--- chippy/opengl.go
+++ chippy/opengl.go
@@ -106,38 +106,44 @@ type GLContext interface {
 }
 
 type GLRenderable interface {
-	// GLConfigs returns all possible OpenGL configurations, these are valid configurations that
-	// may be used in an call to GLSetConfig.
+	// GLConfigs returns all possible OpenGL configurations, these are valid
+	// configurations that may be used in an call to GLSetConfig.
 	GLConfigs() []*GLConfig
 
-	// GLSetConfig sets the OpenGL framebuffer configuration, unlike other window management
-	// libraries, this action may be performed multiple times.
+	// GLSetConfig sets the OpenGL framebuffer configuration, unlike other
+	// window management libraries, this action may be performed multiple
+	// times.
 	//
-	// The config parameter must be an *GLConfig that originally came from the GLConfigs() function
-	// mainly do to the fact that it must be initialized internally.
+	// The config parameter must be an *GLConfig that originally came from the
+	// GLConfigs() function mainly do to the fact that it must be initialized
+	// internally.
 	GLSetConfig(config *GLConfig)
 
-	// GLConfig returns the currently in use *GLConfig or nil, as it was previously set via an call
-	// to GLSetConfig()
+	// GLConfig returns the currently in use *GLConfig or nil, as it was
+	// previously set via an call to GLSetConfig()
 	GLConfig() *GLConfig
 
-	// GLCreateContext creates an OpenGL context for the specified OpenGL version, or returns
-	// an error in the event that we cannot create an context for that version.
+	// GLCreateContext creates an OpenGL context for the specified OpenGL
+	// version, or returns an error in the event that we cannot create an
+	// context for that version.
 	//
-	// The flags parameter may be any combination of the predifined flags, as follows:
+	// The flags parameter may be any combination of the predifined flags, as
+	// follows:
 	//
 	// GLDebug, you will receive an OpenGL debug context. *
 	//
-	// GLForwardCompatible, you will receive an OpenGL forward compatible context. *
+	// GLForwardCompatible, you will receive an OpenGL forward compatible
+	// context. *
 	//
 	// GLCoreProfile, you will receive an OpenGL core context.
 	//
-	// GLCompatibilityProfile, you will receive an OpenGL compatibility context.
+	// GLCompatibilityProfile, you will receive an OpenGL compatibility
+	// context.
 	//
 	// Only one of GLCoreProfile or GLCompatibilityProfile should be present.
 	//
-	// GLCompatabilityProfile will be used if neither GLCoreProfile or GLCompatibilityProfile are
-	// present, or if both are present.
+	// GLCompatabilityProfile will be used if neither GLCoreProfile or
+	// GLCompatibilityProfile are present, or if both are present.
 	//
 	// * = It is not advised to use this flag in production.
 	//
@@ -149,24 +155,29 @@ type GLRenderable interface {
 	// The context to destroy must not be active in any thread, period.
 	GLDestroyContext(c GLContext)
 
-	// GLMakeCurrent makes the specified context the current, active OpenGL context in the current
-	// operating system thread.
+	// GLMakeCurrent makes the specified context the current, active OpenGL
+	// context in the current operating system thread.
 	//
-	// To make the OpenGL context inactive, you may call this function using nil as the context,
-	// which will release the context.
+	// To make the OpenGL context inactive, you may call this function using
+	// nil as the context, which will release the context.
 	//
-	// This function may be called from any thread, but an OpenGL context may only be active inside
-	// one thread at an time.
+	// This function may be called from any thread, but an OpenGL context may
+	// only be active inside one thread at an time.
 	GLMakeCurrent(c GLContext)
 
 	// GLSwapBuffers swaps the front and back buffers of this Renderable.
 	//
-	// This function may only be called in the presence of an active OpenGL context.
+	// This function may only be called in the presence of an active OpenGL
+	// context.
 	//
-	// If the GLConfig set previously via GLSetConfig() is not DoubleBuffered, then this function
-	// is no-op.
+	// If the GLConfig set previously via GLSetConfig() is not DoubleBuffered,
+	// then this function is no-op.
 	GLSwapBuffers()
 
 	// GLSetVerticalSync sets the vertical refresh rate sync mode (vsync).
+	//
+	// This function should only be called in the presence of an active OpenGL
+	// context or else the call may fail due to drivers or platforms that
+	// require an active context (e.g. Mesa).
 	GLSetVerticalSync(mode VSyncMode)
 }
