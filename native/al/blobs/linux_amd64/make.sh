3d22a084b47f38b6696798ee5bbb7d8fb58ae9aa
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2013-12-26 00:28:17 -0700

    Revert "native/al: move to more idiomatic way of creating blobs" - issues on linux
    This reverts commit 27e5e3c95553e1efc8366e4c90835e403bd0c768.

--- native/al/blobs/linux_amd64/make.sh
+++ native/al/blobs/linux_amd64/make.sh
@@ -1,1 +1,1 @@
-as openal.S -o ../../blob_linux_amd64.syso
+objcopy -I binary -O elf64-x86-64 -B i386 openal ../../blob_linux_amd64.syso
--- native/al/extract.go
+++ native/al/extract.go
@@ -5,14 +5,27 @@
 package al
 
 /*
-extern char _blobopenal[], _eblobopenal;
+extern void* openal_data[] asm("_binary_openal_start");
 
-void* get_openal_data() {
-	return _blobopenal;
+#if defined(_WIN32) || defined(_WIN64)
+	// This stub is to fix a compilation error with Go 1.2 on Windows.
+	//
+	// It is likely that we can remove this once the external linker is used on
+	// Windows in Go 1.3, right now this is just kind of a hack to get it to
+	// compile OK with Go 1.2.
+	int _binary_openal_size;
+	void* openal_data_size = (void*)1619557; // hard-coded file size in bytes
+#else
+	//extern void* openal_data_size[] asm("_binary_openal_size");
+#endif
+
+
+void* get_openal_data(void) {
+	return (void*)openal_data;
 }
 
-void* get_openal_data_size() {
-	return &_eblobopenal;
+void* get_openal_data_size(void) {
+	return (void*)openal_data_size;
 }
 */
 import "C"
@@ -34,11 +47,10 @@ var (
 
 func init() {
 	// Initialize blob slice using blob data
-	ptrData := C.get_openal_data()
-	ptrSize := C.get_openal_data_size()
-	sz := int(uintptr(ptrSize) - uintptr(ptrData))
+	fsz := C.get_openal_data_size()
+	sz := *(*int)(unsafe.Pointer(&fsz))
 	sliceHeader := (*reflect.SliceHeader)(unsafe.Pointer(&blob))
-	sliceHeader.Data = uintptr(ptrData)
+	sliceHeader.Data = uintptr(C.get_openal_data())
 	sliceHeader.Len = sz
 	sliceHeader.Cap = sz
 
