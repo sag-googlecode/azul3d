76cf4bc6fc5a7aee5dea91833e531eabf3319a2d
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2014-01-19 18:44:04 -0700

    scene/renderer/gl: regionToGL() simply a bit and avoid unnecessary type conversions

--- scene/renderer/gl/coordsys.go
+++ scene/renderer/gl/coordsys.go
@@ -13,31 +13,27 @@ var (
 	coordSysConversion = math.CoordSysZUpRight.ConvertMat4(math.CoordSysYUpRight)
 )
 
-func regionToGL(totalWidth, totalHeight, x, y, width, height uint) (glX, glY, glWidth, glHeight int32) {
-	var rx, ry, rWidth, rHeight uint
+func regionToGL(totalWidth, totalHeight, x, y, width, height uint) (glX, glY int32, glWidth, glHeight uint32) {
 	if x == 0 && y == 0 && width == 0 && height == 0 {
-		rWidth = totalWidth
-		rHeight = totalHeight
-	} else {
-		rx = x
-		ry = totalHeight - (y + height)
-
-		rWidth = width
-		rHeight = height
-
-		if rx < 0 {
-			rx = 0
-		}
-		if ry < 0 {
-			ry = 0
-		}
-		if rWidth > totalWidth {
-			rWidth = totalWidth
-		}
-		if rHeight > totalHeight {
-			rHeight = totalHeight
-		}
+		// A region of all zeros means the entire area
+		return 0, 0, uint32(totalWidth), uint32(totalHeight)
 	}
 
-	return int32(rx), int32(ry), int32(rWidth), int32(rHeight)
+	// Flip Y axis and leave X axis untouched
+	glX = int32(x)
+	glY = int32(totalHeight - (y + height))
+	if glY < 0 {
+		glY = 0
+	}
+
+	// Verify max sizes
+	if width > totalWidth {
+		width = totalWidth
+	}
+	if height > totalHeight {
+		height = totalHeight
+	}
+	glWidth = uint32(width)
+	glHeight = uint32(height)
+	return
 }
