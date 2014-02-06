21e087053d2fbaa65b9ad180bcaaa16edd331c5c
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2014-02-06 00:21:41 -0700

    scene: fix godoc synopsis

---
+++ scene/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2014 Lightpoke. All rights reserved.
+// This source code is subject to the terms and
+// conditions defined in the "License.txt" file.
+
+// Package scene implements scene graph related data types.
+package scene
