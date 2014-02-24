f7ec04230af18f45d2c60dba7eb486594b5f0f3b
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2014-02-24 13:31:53 -0700

    native/freetype: statically link against libm again (see Go issue 7367)

--- native/freetype/linux/amd64/makesyso.sh
+++ native/freetype/linux/amd64/makesyso.sh
@@ -3,6 +3,6 @@ ar x libbz2.a
 ar x libfreetype.a
 ar x libpng.a
 
-ld -r *.o -o ../../freetype_linux_amd64.syso
+ld -r *.o /usr/lib/x86_64-linux-gnu/libm.a -o ../../freetype_linux_amd64.syso
 
 rm -rf *.o
