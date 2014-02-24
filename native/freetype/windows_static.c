209e5eccc8298906f527826262db82917ad57464
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2014-02-24 14:51:38 -0700

    native: update license headers

--- native/freetype/windows_static.c
+++ native/freetype/windows_static.c
@@ -1,3 +1,7 @@
+// Copyright 2014 The Azul3D Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // This stub is to fix a compilation error with Go 1.2
 //
 // It is likely that we can remove this once the external linker is used on
