560313fc74e6f2c6e69ac3e88d6766971bab403f
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2014-03-31 10:38:23 -0700

    update some missing license headers

--- doc.go
+++ doc.go
@@ -1,3 +1,7 @@
+// Copyright 2014 The Azul3D Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // This is here to avoid go get complaining about no buildable Go source files.
 
 // Azul3D version 1.
