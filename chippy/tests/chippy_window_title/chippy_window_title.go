0c319eb637166378747046ab5a5b97730478da7c
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2014-03-31 10:33:51 -0700

    chippy: update license headers

--- chippy/tests/chippy_window_title/chippy_window_title.go
+++ chippy/tests/chippy_window_title/chippy_window_title.go
@@ -1,6 +1,6 @@
-// Copyright 2012 Lightpoke. All rights reserved.
-// This source code is subject to the terms and
-// conditions defined in the "License.txt" file.
+// Copyright 2014 The Azul3D Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 // +build tests
 
 // Test - Opens two windows, changes their title properties.
