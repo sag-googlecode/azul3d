425adf614a63dfe5831647cb71d26483a074e18a
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2014-03-31 10:24:32 -0700

    audio: use a doc.go file for documentation; update license headers

--- audio/audio.go
+++ audio/audio.go
@@ -1,10 +1,7 @@
-// Copyright 2012 Lightpoke. All rights reserved.
-// This source code is subject to the terms and
-// conditions defined in the "License.txt" file.
+// Copyright 2014 The Azul3D Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 
-// Package audio implements various audio types and interfaces.
-//
-// This package aims to be like the 'image' package, except for audio.
 package audio
 
 import (
