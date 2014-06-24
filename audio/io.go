bd424bda26dcb47ebfa7cd5c76afe217adb73866
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2014-06-24 02:28:30 -0700

    audio: add Reader/Writer Copy function; add WriterTo/ReaderFrom

--- audio/io.go
+++ audio/io.go
@@ -4,6 +4,8 @@
 
 package audio
 
+import "io"
+
 // Reader is a generic interface which describes any type who can have audio
 // samples read from it into an audio slice.
 type Reader interface {
@@ -49,4 +51,75 @@ type Writer interface {
 	// If any error is returned, it should be considered as fatal to the
 	// writer, no more data can subsequently be wrote to the writer.
 	Write(b Slice) (wrote int, err error)
+}
+
+// WriterTo is the interface that wraps the WriteTo method.
+//
+// WriteTo writes data to w until there's no more data to write or when an
+// error occurs. The return value n is the number of samples written. Any error
+// encountered during the write is also returned.
+//
+// The Copy function uses WriterTo if available.
+type WriterTo interface {
+	WriteTo(w Writer) (n int64, err error)
+}
+
+// ReaderFrom is the interface that wraps the ReadFrom method.
+//
+// ReadFrom reads data from r until io.EOF or error. The return value n is the
+// number of bytes read. Any error except io.EOF encountered during the read is
+// also returned.
+//
+// The Copy function uses ReaderFrom if available.
+type ReaderFrom interface {
+	ReadFrom(r Reader) (n int64, err error)
+}
+
+// Copy copies from src to dst until either io.EOF is reached on src or an
+// error occurs.  It returns the number of samples copied and the first error
+// encountered while copying, if any.
+//
+// A successful Copy returns err == nil, not err == io.EOF. Because Copy is
+// defined to read from src until EOF, it does not treat an io.EOF from Read as
+// an error to be reported.
+//
+// If src implements the WriterTo interface, the copy is implemented by calling
+// src.WriteTo(dst). Otherwise, if dst implements the ReaderFrom interface, the
+// copy is implemented by calling dst.ReadFrom(src).
+func Copy(dst Writer, src Reader) (written int64, err error) {
+	// If the reader has a WriteTo method, use it to do the copy. Avoids an
+	// allocation and a copy.
+	if wt, ok := src.(WriterTo); ok {
+		return wt.WriteTo(dst)
+	}
+	// Similarly, if the writer has a ReadFrom method, use it to do the copy.
+	if rt, ok := dst.(ReaderFrom); ok {
+		return rt.ReadFrom(src)
+	}
+	buf := make(F64Samples, (32*1024)/8)
+	for {
+		nr, er := src.Read(buf)
+		if nr > 0 {
+			nw, ew := dst.Write(buf[0:nr])
+			if nw > 0 {
+				written += int64(nw)
+			}
+			if ew != nil {
+				err = ew
+				break
+			}
+			if nr != nw {
+				err = io.ErrShortWrite
+				break
+			}
+		}
+		if er == io.EOF {
+			break
+		}
+		if er != nil {
+			err = er
+			break
+		}
+	}
+	return written, err
 }
